adbc589fa2f2c3dc7af8073810136157b810c669
hija <hilko.j@wwu.de>
2020-04-25 14:25:35 +0200

    Start adding questionnaire logic

--- backend/toolmatching/__init__.py
+++ backend/toolmatching/__init__.py
@@ -1,6 +1,8 @@
 import os
+import json
 
-from flask import Flask
+from flask import Flask, jsonify, session, request
+
 
 def create_app(test_config=None):
     # create and configure the app
@@ -23,9 +25,44 @@ def create_app(test_config=None):
     except OSError:
         pass
 
+    # load questionnaire data
+    with open(app.config['QUESTIONNAIRE_CATEGORIES']) as questionnaire_categories_file:
+        questionnaire_categories_data = json.load(questionnaire_categories_file)
+
+    category_data_dict = dict()
+    for category in questionnaire_categories_data['categories']:
+        with open(os.path.join(app.instance_path, category['data'])) as data:
+            category_data_dict[category['name']] = json.load(data)
+
     # a simple page that says hello
     @app.route('/hello')
     def hello():
         return 'Hello, World!'
 
+    @app.route('/categories')
+    def get_categories():
+        categories_data = {}
+        for category_data in questionnaire_categories_data['categories']:
+            categories_data[category_data['name']] = {'endpoint': 'questionnaire/' + category_data['endpoint']}
+        return jsonify(categories_data)
+
+    def get_question(category, questionid):
+        return
+
+    @app.route('/questionnaire', methods=['POST'])
+    def process_questionnaire():
+        ## Check if questionnaire catergory exists --> Otherwise return error
+        if request.values.get('category'):
+            # This is just once called at the beginning --> We reset the session
+            session.clear()
+            category = request.values.get('category')
+            if not(any(category_data['endpoint'] == category for category_data in questionnaire_categories_data['categories'])):
+                return jsonify({'error': 'Unknown category. Please restart. In the worst case, please choose another category.'})
+            session['category'] = request.value.get('category')
+            session['question'] = 0
+        elif request.values.get('id') and request.values.get('response'):
+            return jsonify({'text': 'SAVING'})
+            session['question'] += 1 # Increase current questionnumber
+        else:
+            return jsonify({'error': 'Invalid request'})
     return app
